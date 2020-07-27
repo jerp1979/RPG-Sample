d9c1376cad8d2c55213e64c6133b7b3f52fb6898
David Long <davdaarn@users.noreply.github.com>
2020-07-27 12:09:57 -0600

    Added basic click to move

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -3,12 +3,38 @@ using UnityEngine.AI;
 
 public class Move : MonoBehaviour
 {
-  [SerializeField] GameObject target;
+  // [SerializeField] GameObject target;
+  [SerializeField] Camera camera;
+
+  Ray lastRay;
 
   NavMeshAgent agent;
   private void Start()
   {
     agent = GetComponent<NavMeshAgent>();
-    agent.destination = target.transform.position;
+    // agent.destination = target.transform.position;
+  }
+
+  private void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+      MoveToCursor();
+    }
+    Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+  }
+
+  private void MoveToCursor()
+  {
+    Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+    // Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    RaycastHit hit;
+
+    bool hasHit = Physics.Raycast(ray, out hit);
+
+    if (hasHit)
+    {
+      agent.destination = hit.point;
+    }
   }
 }
